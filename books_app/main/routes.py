dbd4a103ae3824baeedcceb7f09257716eeaf273
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-04-28 17:00:51 -0400

    added @login_required to the necessary routes

--- books_app/main/routes.py
+++ books_app/main/routes.py
@@ -23,6 +23,7 @@ def homepage():
 
 
 @main.route('/create_book', methods=['GET', 'POST'])
+@login_required
 def create_book():
     form = BookForm()
 
@@ -44,6 +45,7 @@ def create_book():
 
 
 @main.route('/create_author', methods=['GET', 'POST'])
+@login_required
 def create_author():
     # TODO: Make an AuthorForm instance
 
@@ -57,6 +59,7 @@ def create_author():
 
 
 @main.route('/create_genre', methods=['GET', 'POST'])
+@login_required
 def create_genre():
     # TODO: Make a GenreForm instance
 
@@ -91,8 +94,8 @@ def profile(username):
     return render_template('profile.html', username=username)
 
 
-# TODO: Add `@login_required`
 @main.route('/favorite/<book_id>', methods=['POST'])
+@login_required
 def favorite_book(book_id):
     book = Book.query.get(book_id)
     # TODO: If the book is not already in user's favorites, then add it,
@@ -102,8 +105,8 @@ def favorite_book(book_id):
     return "Not yet implemented!"
 
 
-# TODO: Add `@login_required`
 @main.route('/unfavorite/<book_id>', methods=['POST'])
+@login_required
 def unfavorite_book(book_id):
     # TODO: If the book is in user's favorites, then remove it,
     # commit the change to the database, and flash a success message.
