71537caf27105147e5e805b5e78e8dfb4f26dd82
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-04-28 16:58:49 -0400

    finished login/logout route, updated views accordingly

--- books_app/auth/routes.py
+++ books_app/auth/routes.py
@@ -26,10 +26,15 @@ def signup():
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    # TODO: Fill out this route!
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        return redirect(next_page if next_page else url_for('main.homepage'))
+    return render_template('login.html', form=form)
 
 @auth.route('/logout')
 def logout():
-    # TODO: Fill out this route!
-    pass
+    logout_user()
+    return redirect(url_for('main.homepage'))
